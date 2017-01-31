568986e3106cd4cdb72dc76b62aa0da0ab2c1a58
Jonathan Garvey <jon.d.garvey@gmail.com>
2017-01-31 16:08:56 -0700

    gut specs

--- src/app/examples/routed/activated-route-stub.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-
-@Injectable()
-export class ActivatedRouteStub {
-  private subject = new BehaviorSubject({subject: 'planet'});
-  params = this.subject.asObservable();
-}
